f5fb25826b6fff6869dae3cb6ab8ebd3725f1f8c
Bojan Mijic <bojanstven@gmail.com>
2025-05-13 21:38:27 +0200

    v1.0 Initial commit

---
+++ FMradio/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  FMradio
+//
+//  Created by Bojan Mijic on 13.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+        ContentView()
+    }
---
+++ FMradio/FMradioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FMradioApp.swift
+//
+//  Created by Bojan Mijic on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FMradioApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
